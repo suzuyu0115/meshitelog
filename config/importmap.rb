df7a283360e140076cf8d12f4048632db5ea3057
suzuyu0115 <szkyut0115@gmail.com>
2023-06-16 19:00:42 +0900

    Add: import.rbの追加

---
+++ config/importmap.rb
@@ -0,0 +1,5 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
