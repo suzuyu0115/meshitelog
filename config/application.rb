cf95f2f9c9c95a080db3f865b1ac4be09e36c320
suzuyu0115 <szkyut0115@gmail.com>
2023-06-17 23:04:47 +0900

    Fix: コードの修正

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,6 @@ module Meshitelog
     end
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    config.assets.enabled = true
   end
 end
