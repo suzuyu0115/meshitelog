8708608f4f8d186ff047f0a415edd5cdbd936227
suzuyu0115 <szkyut0115@gmail.com>
2023-06-30 00:10:03 +0900

    Add: キャッシュの削除

--- app/javascript/application.js
+++
@@ -1,8 +0,0 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
-
-import jquery from "jquery"
-window.$ = window.jQuery = jquery
-window.bootstrap = require("bootstrap")
--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  OmniAuth.config.allowed_request_methods = [:get]
-  provider :line, ENV['LINE_CHANNEL_ID'], ENV['LINE_CHANNEL_SECRET']
-end
