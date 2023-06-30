4cf20926ce8b005ac6753052aff62e29f2835e33
suzuyu0115 <szkyut0115@gmail.com>
2023-06-30 16:26:27 +0900

    Add: postsにuserのアソシエーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,15 @@
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
