a61713869521bbd5f40021a89fe7bf000e17967f
suzuyu0115 <szkyut0115@gmail.com>
2023-06-30 16:33:47 +0900

    Edit: コントローラービューの編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,6 +18,8 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Post < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 65_535 }
 end
