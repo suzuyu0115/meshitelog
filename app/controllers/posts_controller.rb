6e32e7811fe7225684cc82e2f0ed54d26b62bc03
suzuyu0115 <szkyut0115@gmail.com>
2023-07-01 19:06:53 +0900

    Edit: postsコントローラの編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.includes(:user).order(created_at: :desc)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, success: t('defaults.message.created', item: Post.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_created', item: Post.model_name.human)
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
