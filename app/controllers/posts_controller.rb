6c0a92d31e394b1cab8f5963bc9775a07e4ad414
suzuyu0115 <szkyut0115@gmail.com>
2023-07-01 20:03:36 +0900

    Add: ビューの編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,8 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to posts_path, success: t('defaults.message.created', item: Post.model_name.human)
     else
-      flash.now['danger'] = t('defaults.message.not_created', item: Post.model_name.human)
-      render :new
+      flash.now[:danger] = t('defaults.message.not_created', item: Post.model_name.human)
+      render :new, status: :unprocessable_entity
     end
   end
 
