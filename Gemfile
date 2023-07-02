5b5b291275685007fb80fbfb5d855543dad09eb5
suzuyu0115 <szkyut0115@gmail.com>
2023-07-02 18:02:31 +0900

    Edit: 感発環境と本番環境で画像の保存先を変更

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ gem 'rails-i18n'
 
 # 画像アップロード機能
 gem 'carrierwave'
+gem 'fog-aws'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- app/uploaders/food_image_uploader.rb
+++ app/uploaders/food_image_uploader.rb
@@ -4,8 +4,15 @@ class FoodImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
+  if Rails.env.development? # 開発環境の場合
+    storage :file
+  elsif Rails.env.test? # テスト環境の場合
+    storage :file
+  else # 本番環境の場合
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
