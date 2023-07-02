366c4bfc39878d9e2dadfbda88600d8bef8435ed
suzuyu0115 <szkyut0115@gmail.com>
2023-07-02 15:53:34 +0900

    Add: postsテーブルにfood_imageカラムを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@
 #
 #  id         :bigint           not null, primary key
 #  content    :text             not null
+#  food_image :string
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_30_072358) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_02_064923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_30_072358) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "food_image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
