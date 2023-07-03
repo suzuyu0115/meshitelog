426bfb187932dfe73cbc3659a4d965b898f28cd7
suzuyu0115 <szkyut0115@gmail.com>
2023-07-03 13:17:36 +0900

    Add: commentモデルの追加

---
+++ app/models/comment.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  body       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_post_id  (post_id)
+#  index_comments_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (post_id => posts.id)
+#  fk_rails_...  (user_id => users.id)
+#
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+end
