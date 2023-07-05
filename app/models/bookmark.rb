effd2c871191600b1dd08dc2532569876ae5e6f1
suzuyu0115 <szkyut0115@gmail.com>
2023-07-05 18:50:20 +0900

    Fix: rubocop

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -23,5 +23,5 @@ class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :user_id, uniqueness: { scope: :post_id }, presence: true
+  validates :user_id, uniqueness: { scope: :post_id }
 end
