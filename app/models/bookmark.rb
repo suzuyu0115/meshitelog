f65bf27463c6ce537597034a4f60d499e05dadeb
suzuyu0115 <szkyut0115@gmail.com>
2023-07-05 15:56:07 +0900

    Add: bookmarkモデルの追加

---
+++ app/models/bookmark.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: bookmarks
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_bookmarks_on_post_id              (post_id)
+#  index_bookmarks_on_user_id              (user_id)
+#  index_bookmarks_on_user_id_and_post_id  (user_id,post_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (post_id => posts.id)
+#  fk_rails_...  (user_id => users.id)
+#
+class Bookmark < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  validates :user_id, uniqueness: { scope: :post_id }, presence: true
+end
