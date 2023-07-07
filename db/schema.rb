703eb8f9b095dc84abf469dd0fe0eb8ca8212d5f
suzuyu0115 <szkyut0115@gmail.com>
2023-07-07 19:12:03 +0900

    Add: usersテーブルにavatarカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_065047) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_07_100900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_065047) do
     t.string "uid", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
