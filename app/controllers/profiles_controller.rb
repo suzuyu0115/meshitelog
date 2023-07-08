e0fe0726f5aa4e599e038feadc88cd5aa55ed45c
suzuyu0115 <szkyut0115@gmail.com>
2023-07-08 19:13:17 +0900

    Edit: なまえ表記の修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,7 +8,7 @@ class ProfilesController < ApplicationController
       redirect_to profile_path, success: t('defaults.message.updated', item: User.model_name.human)
     else
       flash.now[:danger] = t('defaults.message.not_updated', item: User.model_name.human)
-      render :edit, status: :unprocessable_entity
+      render :edit
     end
   end
 
@@ -21,6 +21,6 @@ class ProfilesController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :avatar, :avatar_cache)
+    params.require(:user).permit(:nickname, :avatar, :avatar_cache)
   end
 end
