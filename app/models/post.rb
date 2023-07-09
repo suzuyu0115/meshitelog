20f2390653dc47ca89bd28c4fbeeec2261daece7
suzuyu0115 <szkyut0115@gmail.com>
2023-07-09 17:29:40 +0900

    Add: published_atカラムの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,14 @@
 #
 # Table name: posts
 #
-#  id         :bigint           not null, primary key
-#  content    :text             not null
-#  food_image :string
-#  title      :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :bigint
+#  id           :bigint           not null, primary key
+#  content      :text             not null
+#  food_image   :string
+#  published_at :datetime
+#  title        :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint
 #
 # Indexes
 #
