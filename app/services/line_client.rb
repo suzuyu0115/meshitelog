c3677878755846a162605636a62ef5c5840f5ca1
suzuyu0115 <szkyut0115@gmail.com>
2023-07-16 16:00:51 +0900

    Fix: rubocop

--- app/services/line_client.rb
+++ app/services/line_client.rb
@@ -3,8 +3,8 @@ require 'line/bot'
 class LineClient
   def initialize
     @client = Line::Bot::Client.new do |config|
-      config.channel_secret = ENV["LINE_MESSAGING_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_MESSAGING_CHANNEL_TOKEN"]
+      config.channel_secret = ENV.fetch("LINE_MESSAGING_CHANNEL_SECRET", nil)
+      config.channel_token = ENV.fetch("LINE_MESSAGING_CHANNEL_TOKEN", nil)
     end
   end
 
