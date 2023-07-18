3e3e402b4411f5223774f4088389240ee2c00b08
suzuyu0115 <szkyut0115@gmail.com>
2023-07-18 18:59:15 +0900

    Add: ジョブの修正

--- app/jobs/publish_post_job.rb
+++ app/jobs/publish_post_job.rb
@@ -3,9 +3,6 @@ class PublishPostJob < ApplicationJob
 
   def perform(post)
     post.update(published: true)
-    line_client = LineClient.new
-    post.deliveries.each do |delivery|
-      line_client.push_message(delivery.user.uid, "新しい投稿があります: #{post.title}")
-    end
+    post.notify_line
   end
 end
