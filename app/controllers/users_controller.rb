4374f12bbea7a246d2a2201a9ae6450e0613d080
suzuyu0115 <szkyut0115@gmail.com>
2023-07-28 01:43:58 +0900

    Add: LIFFログインの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,9 @@ class UsersController < ApplicationController
     channel_id = ENV['LINE_CHANNEL_ID']
     res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'), { 'id_token' => id_token, 'client_id' => channel_id })
     line_user_id = JSON.parse(res.body)['sub']
-    user = User.find_by(line_user_id:)
-    if user.nil?
-      user = User.create(line_user_id:)
-    elsif (session[:user_id] = user.id)
-      render json: user
-    end
+    user = User.find_or_create_by(line_user_id: line_user_id)
+    session[:user_id] = user.id
+    render json: user
   end
 
   def destroy
