97a044bb675813f874db03bf620ad175b3d5440c
suzuyu0115 <szkyut0115@gmail.com>
2023-07-27 19:46:51 +0900

    Edit: いいね数の非同期表示

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,12 +2,12 @@ class BookmarksController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     current_user.bookmark(post)
-    redirect_back fallback_location: root_path, success: t('.success')
+    redirect_back fallback_location: root_path
   end
 
   def destroy
     post = current_user.bookmarks.find(params[:id]).post
     current_user.unbookmark(post)
-    redirect_back fallback_location: root_path, success: t('.success')
+    redirect_back fallback_location: root_path
   end
 end
