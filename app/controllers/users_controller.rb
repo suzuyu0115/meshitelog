ccafed10e4b40c27e142800b8e5581b4a6e184bf
suzuyu0115 <szkyut0115@gmail.com>
2023-07-28 16:20:15 +0900

    Fix: rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,22 +3,20 @@ class UsersController < ApplicationController
   require 'uri'
 
   def new
-    if current_user
-      redirect_to posts_path
-    end
+    redirect_to posts_path if current_user
   end
 
   def create
-      id_token = params[:idToken]
-      name = params[:name]
-      channel_id = ENV['LINE_CHANNEL_ID']
-      res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'), { 'id_token' => id_token, 'client_id' => channel_id })
-      line_user_id = JSON.parse(res.body)['sub']
-      @user = User.find_or_create_by(line_user_id: line_user_id) do |user|
-        user.name = name
-      end
-      session[:user_id] = @user.id
-      render json: @user
+    id_token = params[:idToken]
+    name = params[:name]
+    channel_id = ENV.fetch('LINE_CHANNEL_ID', nil)
+    res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'), { 'id_token' => id_token, 'client_id' => channel_id })
+    line_user_id = JSON.parse(res.body)['sub']
+    @user = User.find_or_create_by(line_user_id:) do |user|
+      user.name = name
+    end
+    session[:user_id] = @user.id
+    render json: @user
   end
 
   def destroy
