dd210afc95a28aead8d733c528e11d9e6a8575ba
suzuyu0115 <szkyut0115@gmail.com>
2023-08-29 18:15:17 +0900

    Add: サイトマップの追加

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "https://meshitelog.com"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
@@ -15,13 +15,28 @@ SitemapGenerator::Sitemap.create do
   #
   # Examples:
   #
-  # Add '/articles'
+  # Add '/posts'
   #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+    add posts_path, :priority => 0.7, :changefreq => 'daily'
+    add top_rated_posts_path, :priority => 0.6, :changefreq => 'daily'
+    add recommended_posts_path, :priority => 0.6, :changefreq => 'daily'
+    add received_posts_path, :priority => 0.5, :changefreq => 'daily'
+    add bookmarks_posts_path, :priority => 0.5, :changefreq => 'daily'
+    add scheduled_posts_path, :priority => 0.4, :changefreq => 'daily'
   #
-  # Add all articles:
+  # Add all posts:
   #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
+    Post.find_each do |post|
+      add post_path(post), :lastmod => post.updated_at
+      add edit_post_path(post), :lastmod => post.updated_at
+    end
+
+    add new_post_path, :priority => 0.5, :changefreq => 'daily'
+
+
+    add profile_path
+    add edit_profile_path
+
+    add terms_path, :priority => 0.2, :changefreq => 'monthly'
+    add privacy_path, :priority => 0.2, :changefreq => 'monthly'
 end
