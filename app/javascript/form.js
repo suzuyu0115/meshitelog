1046933a2b3299cf13dc7dd9f07d4e3c69bbfeeb
suzuyu0115 <szkyut0115@gmail.com>
2023-08-30 16:49:40 +0900

    Fix: バリデーションの編集

--- app/javascript/form.js
+++ app/javascript/form.js
@@ -1,33 +1,36 @@
 window.onload = function () {
+  const postForm = document.getElementById('post_form');
   const postSubmit = document.querySelector('.btn.btn-warning.btn-lg');
   const postTitle = document.getElementById('post_title');
   const postContent = document.getElementById('post_content');
   const validationMessages = document.getElementById('validation-messages');
 
-  function validateForm() {
+  function validateForm(event) {
     let valid = true;
     let messages = [];
 
-    // タイトルのバリデーション
     if (postTitle.value.trim() === "") {
       valid = false;
       messages.push("タイトルは必須です");
     }
 
-    // コンテンツのバリデーション
     if (postContent.value.trim() === "") {
       valid = false;
       messages.push("コンテンツは必須です");
     }
 
-    validationMessages.innerHTML = messages.join('<br>');
-    postSubmit.disabled = !valid;
+    validationMessages.innerHTML = '';
+    messages.forEach(message => {
+      const errorElement = document.createElement('div');
+      errorElement.className = 'alert alert-danger';
+      errorElement.textContent = message;
+      validationMessages.appendChild(errorElement);
+    });
+
+    if (!valid) {
+      event.preventDefault();
+    }
   }
 
-  postTitle.addEventListener('blur', validateForm);
-  postContent.addEventListener('blur', validateForm);
-  postFoodImage.addEventListener('change', validateForm);
-
-  // 初期のチェック
-  validateForm();
+  postForm.addEventListener('submit', validateForm);
 };
