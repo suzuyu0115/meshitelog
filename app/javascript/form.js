fb0a9c157516f0c8bcc3d1530a48643a0f2af24e
suzuyu0115 <szkyut0115@gmail.com>
2023-09-14 17:12:04 +0900

    Fix: バリデーションの修正

--- app/javascript/form.js
+++ app/javascript/form.js
@@ -3,29 +3,28 @@ document.addEventListener('turbo:load', function () {
   const postSubmit = document.querySelector('.btn.btn-warning.btn-lg');
   const postTitle = document.getElementById('post_title');
   const postContent = document.getElementById('post_content');
-  const validationMessages = document.getElementById('validation-messages');
+  // const validationMessages = document.getElementById('validation-messages'); この行は不要です
 
   function validateForm(event) {
     let valid = true;
-    let messages = [];
 
     if (postTitle.value.trim() === "") {
       valid = false;
-      messages.push("飯名は必須です");
+      postTitle.classList.add('is-invalid');
+      postTitle.classList.remove('is-valid');
+    } else {
+      postTitle.classList.add('is-valid');
+      postTitle.classList.remove('is-invalid');
     }
 
     if (postContent.value.trim() === "") {
       valid = false;
-      messages.push("コメントは必須です");
+      postContent.classList.add('is-invalid');
+      postContent.classList.remove('is-valid');
+    } else {
+      postContent.classList.add('is-valid');
+      postContent.classList.remove('is-invalid');
     }
-
-    validationMessages.innerHTML = '';
-    messages.forEach(message => {
-      const errorElement = document.createElement('div');
-      errorElement.className = 'alert alert-danger';
-      errorElement.textContent = message;
-      validationMessages.appendChild(errorElement);
-    });
 
     postSubmit.disabled = !valid;
 
@@ -34,10 +33,8 @@ document.addEventListener('turbo:load', function () {
     }
   }
 
-  // blurとchangeイベントでバリデーションを実行
   postTitle.addEventListener('blur', validateForm);
   postContent.addEventListener('blur', validateForm);
-  postFoodImage.addEventListener('change', validateForm);
 
   postForm.addEventListener('submit', validateForm);
 
